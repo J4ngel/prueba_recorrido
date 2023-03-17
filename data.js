eb7bda01e9490ec9f9e76e611eb4f1bbe070ea58
J4ngel <andres_angel@live.com.ar>
2023-03-17 12:21:06 -0500

    Subida de datos

---
+++ data.js
@@ -0,0 +1,429 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada-y-pasillo",
+      "name": "Entrada y pasillo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.000277864100013403,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-laboratorio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-laboratorio",
+      "name": "Laboratorio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-laboratorio-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-laboratorio-11",
+      "name": "Laboratorio 1(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.01354030739154588,
+          "pitch": 0.16378898549545085,
+          "rotation": 0,
+          "target": "3-laboratorio-12"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-laboratorio-12",
+      "name": "Laboratorio 1(2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.006405780199729705,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-laboratorio-13"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-laboratorio-13",
+      "name": "Laboratorio 1(3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5009805147192701,
+          "pitch": 0.11865510237221066,
+          "rotation": 0,
+          "target": "5-laboratorio-14"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-laboratorio-14",
+      "name": "Laboratorio 1(4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20990813145059306,
+          "pitch": 0.12400773266741716,
+          "rotation": 0,
+          "target": "6-laboratorio-15"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-laboratorio-15",
+      "name": "Laboratorio 1(5)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.028154414215347146,
+          "pitch": 0.10968292769656962,
+          "rotation": 0,
+          "target": "7-laboratorio-16"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-laboratorio-16",
+      "name": "Laboratorio 1(6)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.055039424005830284,
+          "pitch": 0.08233091872053322,
+          "rotation": 0,
+          "target": "8-laboratorio-17"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-laboratorio-17",
+      "name": "Laboratorio 1(7)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.004240350600008824,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "9-laboratorio-18"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-laboratorio-18",
+      "name": "Laboratorio 1(8)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "10-laboratorio-19"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-laboratorio-19",
+      "name": "Laboratorio 1(9)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05709316396886521,
+          "pitch": 0.01718608590573467,
+          "rotation": 0,
+          "target": "0-entrada-y-pasillo"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Recorrido 3 piso",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
